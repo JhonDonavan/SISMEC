fbea34ad1d083b932cef5ca80cbe9fa692e5b87c
Jhon Donavan <jhondonavan@gmail.com>
2017-02-07 16:56:13 -0200

    mensagen no usuariodao

--- src/modelDAO/UsuarioDAO.java
+++ src/modelDAO/UsuarioDAO.java
@@ -7,3 +7,8 @@ import model.Usuario;
 public interface UsuarioDAO extends JpaRepository<Usuario, Integer>{
 
 }
+
+
+
+//seguir para login
+//http://www.devmedia.com.br/java-web-criando-uma-tela-de-login-com-jpa-jsf-primefaces-e-mysql/32456
