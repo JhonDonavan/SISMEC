b5e80d150cef2c075b7b85e79ce9aa32d3d5187f
guig4byte <guig4byte@gmial.com>
2017-02-07 11:48:41 -0200

    Teste git

--- src/teste/TesteClass.java
+++ src/teste/TesteClass.java
@@ -4,6 +4,12 @@ public class TesteClass {
 
 	public static void main(String[] args) {
 		/*Arenas testando o github*/
+		/*Arenas testando o github*/
+		/*Arenas testando o github*/
+		/*Arenas testando o github*/
+		/*Arenas testando o github*/
+		/*Arenas testando o github*/
+		/*Arenas testando o github*/
 
 	}
 
