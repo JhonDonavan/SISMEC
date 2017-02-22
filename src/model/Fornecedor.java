7bfaabd14ba1d3ab4cbce4981b9416f4191cc9e9
guig4byte <guig4byte@gmial.com>
2017-02-22 14:29:06 -0300

    1 pull request

---
+++ src/model/Fornecedor.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+public class Fornecedor implements Serializable{
+	
+	private static final long SerialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "FORNECEDOR_ID", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "FORNECEDOR_ID", sequenceName = "SEQ_FORNECEDOR", allocationSize = 1)
+	private Integer id;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@OneToMany(mappedBy = "FORNECEDOR", targetEntity = Peca.class, 
+			fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Peca> peca;
+	
+	private String nome;
+	
+	private String telfornecedor;
+
+}
