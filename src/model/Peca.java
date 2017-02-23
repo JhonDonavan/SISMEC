f7cef8d4c68758ace6d461a19cd65f94837fa072
guig4byte <guig4byte@gmial.com>
2017-02-23 20:10:04 -0300

    teste 3

--- src/model/Peca.java
+++ src/model/Peca.java
@@ -23,6 +23,34 @@ public class Peca implements Serializable{
 	private String num;
 	
 	private int qtd;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public int getQtd() {
+		return qtd;
+	}
+
+	public void setQtd(int qtd) {
+		this.qtd = qtd;
+	}
 	
 
 }
