43f76952335fd82d40f7f8e9735a6cf98be054b8
guig4byte <guig4byte@gmial.com>
2017-02-23 20:06:49 -0300

    teste

--- src/model/Peca.java
+++ src/model/Peca.java
@@ -2,14 +2,27 @@ package model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Peca implements Serializable{
 	
-	private Long id;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "PECA_ID", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "PECA_ID", sequenceName = "SEQ_PECA", allocationSize = 1)
+	private long id;
 	
 	private String nome;
 	
 	private String num;
 	
 	private int qtd;
+	
 
 }
