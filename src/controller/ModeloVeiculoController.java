3e00bb22463ab3abb39b05ad5c0d977285433a57
Java <Java@DCX-LABD1600082>
2017-03-06 11:25:21 -0300

    construindo Janela listarModelosVeiculo

--- src/controller/ModeloVeiculoController.java
+++ src/controller/ModeloVeiculoController.java
@@ -41,6 +41,8 @@ public class ModeloVeiculoController {
 				new FacesMessage("Modelo de veiculo cadastrado com sucesso!"));
 		System.out.println("Objeto " + modeloVeiculo.getNome() + " salvo com sucesso!");
 		return "cadastrarModeloVeiculo?faces-redirect=true";
+		
+		
 	}
 
 	public List<Montadora> listarPorNome(String nomeMontadora){
@@ -49,6 +51,10 @@ public class ModeloVeiculoController {
 		System.out.println("Entrou no metodo listarPorNome: " + nomeMontadora + " E " + nomeMontadora);
 		for(Montadora m : montadorasSelecionados){
 			System.out.println("Montadora: " + m.getNome() + " + "  + m.getPais() + " + "  + m.getSite());
+		}
+		if(montadorasSelecionados.isEmpty()){
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Montadora de veiculo nao cadastrado."));
 		}
 		return montadorasSelecionados;
 	}
