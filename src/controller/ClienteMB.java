031394b3bade24be94b16260716f82e1e4d166d5
JhonDonavan <jhondonavan@gmail.com>
2017-03-21 11:10:13 -0300

    Editar cliente OK

--- src/controller/ClienteMB.java
+++ src/controller/ClienteMB.java
@@ -11,50 +11,45 @@ import javax.faces.context.FacesContext;
 import model.Cliente;
 import modelDAO.GenericDAO;
 
-@ManagedBean(name="clienteMB")
+@ManagedBean(name = "clienteMB")
 @SessionScoped
 public class ClienteMB {
-	
+
 	private Cliente cliente = new Cliente();
 	private List<Cliente> clientes = new ArrayList<Cliente>();
-	
-	public ClienteMB(){
+
+	public ClienteMB() {
 		cliente = new Cliente();
 		clientes = new GenericDAO<Cliente>(Cliente.class).listarTodos();
 	}
-	
-	
-	public String salvar(){
+
+	public String salvar() {
 		new GenericDAO<Cliente>(Cliente.class).salvar(cliente);
-		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage("Cliente cadastrado com sucesso"));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente cadastrado com sucesso"));
 		System.out.println("Objeto " + cliente.getNome() + " cadastrado com sucesso!");
 		clientes = new GenericDAO<Cliente>(Cliente.class).listarTodos();
-		return "cadastrarCliente?faces-redirect=true";
+		return "listarClientes?faces-redirect=true";
 	}
-
+	
+	public String editar(Cliente cliente){
+		this.cliente = cliente;
+		return "cadastrarCliente.xhtml?faces-redirect=true";
+	}
 
 	public Cliente getCliente() {
 		return cliente;
 	}
 
-
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-
 
 	public List<Cliente> getClientes() {
 		return clientes;
 	}
 
-
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
 	}
-	
-	
-	
-	
-	
+
 }
