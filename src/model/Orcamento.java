36f4ff617d5712ea4a23857151580dbf3eab51f9
guig4byte <guig4byte@gmial.com>
2017-03-23 21:29:53 -0300

    Correções de erros nas classes orcamento, mecanico e pagamento

--- src/model/Orcamento.java
+++ src/model/Orcamento.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -11,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
 @Entity
-public class Orcamento {
+public class Orcamento implements Serializable{
 	
 private static final long SerialVersionUID = 1L;
 	
