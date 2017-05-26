c3df3c4621ad88ef24476e3320269540fe11a93b
Jhon Donavan <jhondonavan@gmail.com>
2017-05-26 09:30:17 -0300

    Modificando permisão de excluir conforme responsabilidade dos usuarios

--- src/security/Seguranca.java
+++ src/security/Seguranca.java
@@ -1,9 +1,13 @@
 package security;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import model.Autorizacao;
 
 @ManagedBean
 public class Seguranca {
@@ -19,16 +23,33 @@ public class Seguranca {
 		return nome;
 	}
 
+	public String getAutorizacao() {
+		String autorizacao = null;
+		
+		List<Autorizacao> autorizacaoes = null;
+
+		UsuarioSistema usuariologado = getUsuarioLogado();
+
+		if (usuariologado != null) {
+			autorizacaoes = usuariologado.getUsuario().getAutorizacoes();
+		}
+		for (Autorizacao user : autorizacaoes) {
+			autorizacao = user.getNome();
+		}
+		return autorizacao;
+
+	}
+
 	private UsuarioSistema getUsuarioLogado() {
 		UsuarioSistema usuario = null;
 
 		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) FacesContext
 				.getCurrentInstance().getExternalContext().getUserPrincipal();
-	
-		if(auth != null && auth.getPrincipal() != null){
+
+		if (auth != null && auth.getPrincipal() != null) {
 			usuario = (UsuarioSistema) auth.getPrincipal();
 		}
-		
+
 		return usuario;
 	}
 
