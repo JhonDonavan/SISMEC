5997a7cf4c7e36e5d929c83aab57e8f74982143f
Jhon Donavan <jhondonavan@gmail.com>
2017-06-10 10:09:17 -0300

    metodo no DAO para retornar funcionarios mecanicos OK e corrigido na O.S

--- src/modelDAO/MecanicoDAO.java
+++ src/modelDAO/MecanicoDAO.java
@@ -10,16 +10,14 @@ import model.Funcionario;
 public class MecanicoDAO {
 
 	
-	/*ESTE MÉTODO DEVE SER ALOCADO NO DAO FuncionarioDAO
-	 * POR FALTA DE TEMPO, FOI ALOCADO AQUI*/
+	/*ESTE METODO DEVE SER ALOCADO EM FuncionarioDAO
+	 * POREM POR FALTA DE TEMPO ESTÁ ALOCADO AQUI*/
 	@SuppressWarnings("unchecked")
 	public List<Funcionario> buscarMecanicoByNome(String nomeFuncionario) {
 		EntityManager em = JPAUtil.getEntityManager();
-		
-		System.out.println(nomeFuncionario);
-		
-		Query query = em.createQuery("SELECT f FROM Funcionario f WHERE upper(f.nome) like upper(:nomeFuncionario)");
+		Query query = em.createQuery("SELECT f FROM Funcionario f WHERE upper(f.nome) like upper(:nomeFuncionario) and f.cargo like :cargo");
 		query.setParameter("nomeFuncionario", "%" + nomeFuncionario + "%");
+		query.setParameter("cargo", "MECANICO");
 		return query.getResultList();
 	}
 
